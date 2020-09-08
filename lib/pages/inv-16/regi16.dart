13d0499f8d7c98db0362e63417c9b0f33d94d9c9
rafai77 <rafac11@hotmail.com>
2020-09-08 11:18:29 -0500

    crear pagina para el 16

---
+++ lib/pages/inv-16/regi16.dart
@@ -0,0 +1,72 @@
+import 'dart:ffi';
+
+class Regi16 {
+  int id_reg;
+  int id_user;
+  int id_inve;
+  int num_tunel;
+  int racimo1;
+  int racimo2;
+  int racimo3;
+  int racimo4;
+  int racimo5;
+  int racimo6;
+  int tamchico;
+  String lado;
+  dynamic peso;
+  int pudricion;
+  int flojo;
+  int mecanico;
+  int blossom;
+  int reventado;
+  int cierre;
+  int deforme;
+  int cicatriz;
+  int insectop; //precencia
+  int insectod; //insecto con daño
+  int craking;
+  int corte;
+  int golpe;
+  int exverde; //extra verde
+  int color_disparejo;
+  int arrudago;
+  int blotchy;
+  int suelto;
+  int viruz;
+  String fecha;
+
+  Regi16(
+    this.id_reg,
+    this.id_user,
+    this.id_inve,
+    this.num_tunel,
+    this.racimo1,
+    this.racimo2,
+    this.racimo3,
+    this.racimo4,
+    this.racimo5,
+    this.racimo6,
+    this.tamchico,
+    this.lado,
+    this.peso,
+    this.flojo,
+    this.mecanico,
+    this.blossom,
+    this.reventado,
+    this.cierre,
+    this.deforme,
+    this.cicatriz,
+    this.insectop, //precencia
+    this.insectod, //insecto con daño
+    this.craking,
+    this.corte,
+    this.golpe,
+    this.exverde, //extra verde
+    this.color_disparejo,
+    this.arrudago,
+    this.blotchy,
+    this.suelto,
+    this.viruz,
+    this.fecha,
+  );
+}
