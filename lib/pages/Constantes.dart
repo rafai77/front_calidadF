d23f5bc9452f298ac8f13922325b502d49f7576f
rafai77 <rafac11@hotmail.com>
2020-09-11 08:08:45 -0500

    final v=1.0

--- lib/pages/Constantes.dart
+++ lib/pages/Constantes.dart
@@ -2,7 +2,8 @@ class Constant {
   //static const String DOMAIN = "http://192.168.1.199:3000";
   //static const String DOMAIN = "http://localhost:3000";
 
-  static const String DOMAIN = "http://192.168.1.129:3000";
+  //static const String DOMAIN = "http://192.168.1.129:3000";
+  static const String DOMAIN = "http://192.168.1.25:3000";
 
   static const String WIFI_DISIABLE = "Sin wifi";
   static const String CONECTION_DISIABLE = "Sin conexion al servidor";
